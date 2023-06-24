3bda4732c7b0832682fe3d078ca1839903eebac1
yungshun317 <seraph317@gmail.com>
2023-06-24 10:25:46 +0800

    [FEAT] Create the turbulent displace effect without using GSAP

--- turbulence/script.js
+++ turbulence/script.js
@@ -1,3 +1,23 @@
 "use strict";
 
 gsap.to("#feTurbulence", 20, { attr: { baseFrequency: 0.04 }, repeat: -1 });
+
+/*
+let frequency = 0.02;
+let element = document.getElementById("feTurbulence");
+
+function animate() {
+    let timing = setInterval(currentAnimationFrame, 1);
+
+    function currentAnimationFrame() {
+        if (frequency > 0.08) {
+            clearInterval(timing);
+        } else {
+            frequency += 0.0001;
+            element.setAttribute("baseFrequency", frequency);
+        }
+    }
+}
+
+animate();
+*/
